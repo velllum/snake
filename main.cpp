4b7528de75dcdab56864e018c8da2423ca428c89
velllum/home/velllum/VSCodeProjects/snake_3_3/build/snake_3 <velllum@ya.ru>
2023-05-06 21:34:25 +0300

    Задача 3

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <cstdio>
 #include <cstdlib>
+
 #include "const.h"
 #include "field/field.h"
 #include "food/food.h"
@@ -12,17 +13,18 @@
 
 int main()
 {
-    setlocale(LC_ALL, "");
+    std::setlocale(LC_ALL, "");
 
-    system("setterm -cursor off");
-    system("stty -icanon -echo"); // включаем неканонический ввод
+    std::system("setterm -cursor off");
+    std::system("stty -icanon -echo"); // включаем неканонический ввод
 
     snake_size =5; // Текущий размер змейки >0 Включает голову+размер хвоста (если 1- то только голова)
     timeout =1000000; // время на сон равное 1 сек.
     game_on =true; // Признак продолжения игры
-    // snake_x[0] =snake_size; // установить индекс головы по центру массива поля
 
     char ch; // получить направление движения змейки
+    int snake_tail; // последнее значение в хвосте
+    int snake_head; // перевое значение, голова
 
 
     /* ДВИЖЕНИЕ */
@@ -33,37 +35,41 @@ int main()
 
     do // задать направление
     {
-        system("clear"); // очистить поле
+        std::system("clear"); // очистить поле
 
-        std::wcout << L"Задайте направление движения 'Змейки': ";
-        std::wcout << "(A, a) - " << WC_LEFT << "; ";
+        // печатаем пользовательскую информацию выбора, направления движения
+        std::wcout << L"Задайте направление движения 'Змейки':\n\n";
+        std::wcout << "(A, a) - " << WC_LEFT << std::endl;
         std::wcout << "(D, d) - " << WC_RIGHT << std::endl;
 
-        ch = getchar();
+        ch =getchar();
 
         switch(ch) // управление через зарезервированные клавиши
         {
             case RIGHT_L: case RIGHT_U: // движение в право
+
                 direction =RIGHT_; // задаем направление движения змейки
                 snake_x[0] =snake_size; // установить индекс головы, с учетом размера змейки
 
                 for (int i =1; i <snake_size; ++i) // заполнить массив змейки хвостом
                 {
-                    snake_x[i] =snake_x[0] -i; // переопределяем значения массива
+                    snake_x[i] =snake_x[0] -i;
                 }
 
                 break;
-            case LEFT_L: case LEFT_U: // движение влево
-                direction =LEFT_; // задаем направление движения змейки
 
+            case LEFT_L: case LEFT_U: // движение влево
+
+                direction =LEFT_; // задаем направление движения змейки
                 snake_x[0] =COLUMNS -snake_size; // установить индекс головы, с учетом размера змейки
 
                 for (int i =1; i <snake_size; ++i) // заполнить массив змейки хвостом
                 {
-                    snake_x[i] =snake_x[0] +i; // переопределяем значения массива
+                    snake_x[i] =snake_x[0] +i;
                 }
 
                 break;
+
             case 27: case 'q': // выход из программы
                 exit(0);
         }
@@ -74,69 +80,74 @@ int main()
     /* УПРАВЛЕНИЕ */
     do // создаем передвижение змейки
     {
-
-        for (int i =0; i <snake_size; ++i) // создаем передвижение змейки
+        switch(direction)
         {
-            switch(direction)
-            {
-                case RIGHT_: // движение в право
+            case RIGHT_: // движение в право
 
-                    // TODO: Проверить за максимальные рамки
+                snake_tail =(snake_x[LEN_SNAKE_MAX -snake_size] -1); // получить последнее значение за хвостом
+                snake_head =snake_x[0]; // получить перевое значение, голову
+
+                for (int i =0; i <snake_size; ++i) // создаем передвижение змейки
+                {
+                    field[snake_x[i]] =TAIL_SYMBOL; // определяем тело на поле
 
                     if (snake_x[i] ==COLUMNS)
                     {
-                        field[snake_x[snake_size -1] -1] =FIELD_SYMBOL; // переопределяем оствшееся значение в поле за хвостом
-                        snake_x[i] =0; // обнуляем первое значение в массиве координат с индексами
-                        continue;
+                        snake_x[i] =0;
                     }
-
-                    else if (i ==0)
+                    else
                     {
-                        field[snake_x[i]++] =HEAD_SYMBOL; // передаем символ головы в массив
-                        continue;
+                        snake_x[i]++;
                     }
-                    
-                    field[snake_x[i]++] =TAIL_SYMBOL; // передаем символ хвоста в массив
-                    field[(snake_x[i] -snake_size) +1] =FIELD_SYMBOL; // переопределяем за хвостом значениями поля
-                    
-                    break;
+                }
 
-                case LEFT_: // движение в лево
+                field[snake_head] =HEAD_SYMBOL; // передаем символ головы в массив
+                field[snake_tail] =FIELD_SYMBOL; // меняем послед значение за хвастом
 
-                    // TODO: ошибка выхода значения массива snake_x за ноль -1
+                break;
+            
+            case LEFT_: // движение в лево
+
+                snake_tail =(snake_x[LEN_SNAKE_MAX -snake_size] +1); // получить последнее значение за хвостом
+                snake_head =snake_x[0]; // получить перевое значение, голову
+
+                for (int i =0; i <snake_size; ++i) // создаем передвижение змейки
+                {
+                    field[snake_x[i]] =TAIL_SYMBOL; // определяем тело на поле
 
                     if (snake_x[i] ==0)
                     {
-                        field[snake_x[snake_size -i]] =FIELD_SYMBOL; // переопределяем оствшееся значение в поле за хвостом
-                        snake_x[i] =COLUMNS; // обнуляем первое значение в массиве координат с индексами
-                        continue;
+                        snake_x[i] =COLUMNS;
                     }
-
-                    else if (i ==0)
+                    else
                     {
-                        field[--snake_x[i]] =HEAD_SYMBOL; // передаем символ головы в массив
-                        continue;
+                        snake_x[i]--;
                     }
+                }
 
-
-                    field[--snake_x[i]] =TAIL_SYMBOL; // передаем символ хвоста в массив
-                    field[(snake_x[i] +snake_size) -1] =FIELD_SYMBOL; // переопределяем за хвостом значениями поля
-
-                    break;
-            }
+                field[snake_head] =HEAD_SYMBOL; // передаем символ головы в массив
+                field[snake_tail] =FIELD_SYMBOL; // меняем послед значение за хвастом
+                
+                break;
         }
 
-        system("clear"); // очистить поле
+        std::system("clear"); // очистить поле
+
+        std::cout << std::endl;
 
         for (int j =0; j <COLUMNS; ++j) // печать результата
         {
-            std::wcout << field[j];
+            std::wcout << field[j] << " ";
         }
+
+        std::wcout << "\t" << L"Направление: " << static_cast<wchar_t>(direction) << std::endl;
+        std::wcout << "\t\t\t" << L"Размер: " << snake_size << std::endl;
+
         std::cout << std::endl;
 
         usleep(timeout); // засыпаем на 1 сек =1000000
 
-         if (kbhit() && ((ch =getchar()) =='q' || (ch =getchar()) ==27))
+        if (kbhit() && ((ch =getchar()) =='q' || (ch =getchar()) ==27))
         {
             game_on =false;
         }
